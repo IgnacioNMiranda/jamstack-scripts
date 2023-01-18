ad14d3ba459b08969793afd346f3723c1effc11b
IgnacioNMiranda <i.miranda.f99@gmail.com>
2023-01-18 10:25:49 -0300

    feat: add limit to delete_entries contentful query

--- src/scripts/contentful/delete-entries.ts
+++ src/scripts/contentful/delete-entries.ts
@@ -4,22 +4,25 @@ import { contentfulClient } from '../../clients'
 // String array
 const ids = ['1', '2', '3']
 
-contentfulClient.getSpace(environment.contentful.spaceId).then((space) => {
-  space.getEnvironment(environment.contentful.env).then((env) => {
-    env
-      .getEntries({
-        content_type: environment.contentful.contentType,
-        // 'sku' or any field that has to match certain criteria
-        'fields.sku[in]': ids.join(','),
-      })
-      .then(async (entries) => {
-        const responses = entries.items.map(async (entry) => {
-          if (entry.sys.publishedAt) {
-            await entry.unpublish()
-          }
-          return env.deleteEntry(entry.sys.id)
-        })
-        await Promise.all(responses)
-      })
-  })
+const limit = 25
+contentfulClient.getSpace(environment.contentful.spaceId).then(async (space) => {
+  const env = await space.getEnvironment(environment.contentful.env)
+  let entriesDeleted = 0
+  while (true) {
+    const entries = await env.getEntries({
+      content_type: environment.contentful.contentType,
+      'fields.sku[in]': ids.join(','),
+      limit,
+      skip: entriesDeleted,
+    })
+    if (entries.items.length === 0) return
+    const responses = entries.items.map(async (entry) => {
+      if (entry.sys.publishedAt) {
+        await entry.unpublish()
+      }
+      return env.deleteEntry(entry.sys.id)
+    })
+    await Promise.all(responses)
+    entriesDeleted += limit
+  }
 })
