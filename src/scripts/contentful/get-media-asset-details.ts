ef9a6c0fb85c7863b1ac50f2b2b6d7a32c5707d6
IgnacioNMiranda <ignacio.miranda@reign.cl>
2023-09-04 16:11:25 -0300

    feat: add pagination in get-media-asset-details script

--- src/scripts/contentful/get-media-asset-details.ts
+++ src/scripts/contentful/get-media-asset-details.ts
@@ -3,40 +3,61 @@ import { contentfulClient } from '../../clients'
 import { createWriteStream } from 'fs'
 import { stringify } from 'csv-stringify'
 
+const limit = 1000
+
 /**
  * Get details of all the media assets present in a given environment in CSV format.
  */
 export default async () => {
   const space = await contentfulClient.getSpace(environment.contentful.spaceId)
   const env = await space.getEnvironment(environment.contentful.env)
-  const mediaAssets = await env.getAssets()
-  const normAssets = mediaAssets.items.map((mediaAsset) => {
-    return {
-      title: mediaAsset.fields.title[environment.contentful.locale],
-      ...(mediaAsset.fields.description && {
-        description: mediaAsset.fields.description[environment.contentful.locale],
-      }),
-      ...(mediaAsset.fields.file && {
-        url: mediaAsset.fields.file[environment.contentful.locale].url,
-        ...(mediaAsset.fields.file[environment.contentful.locale].details?.size && {
-          size: mediaAsset.fields.file[environment.contentful.locale].details?.size,
-        }),
-        ...(mediaAsset.fields.file[environment.contentful.locale].details?.image?.width && {
-          width: mediaAsset.fields.file[environment.contentful.locale].details?.image?.width,
-        }),
-        ...(mediaAsset.fields.file[environment.contentful.locale].details?.image?.height && {
-          height: mediaAsset.fields.file[environment.contentful.locale].details?.image?.height,
-        }),
-        fileName: mediaAsset.fields.file[environment.contentful.locale].fileName,
-        contentType: mediaAsset.fields.file[environment.contentful.locale].contentType,
-      }),
-    }
-  })
+
+  let assetsFetched = 0
+  const totalAssets = []
+  while (true) {
+    const mediaAssets = await env.getAssets({
+      limit,
+      skip: assetsFetched,
+    })
+
+    if (mediaAssets.items.length === 0) break
+
+    const normAssets = mediaAssets.items
+      .map((mediaAsset) => {
+        if (Object.keys(mediaAsset.fields).length === 0) return undefined
+        return {
+          id: mediaAsset.sys.id,
+          title: mediaAsset.fields.title[environment.contentful.locale],
+          ...(mediaAsset.fields.description && {
+            description: mediaAsset.fields.description[environment.contentful.locale],
+          }),
+          ...(mediaAsset.fields.file && {
+            url: mediaAsset.fields.file[environment.contentful.locale].url,
+            ...(mediaAsset.fields.file[environment.contentful.locale].details?.size && {
+              size: mediaAsset.fields.file[environment.contentful.locale].details?.size,
+            }),
+            ...(mediaAsset.fields.file[environment.contentful.locale].details?.image?.width && {
+              width: mediaAsset.fields.file[environment.contentful.locale].details?.image?.width,
+            }),
+            ...(mediaAsset.fields.file[environment.contentful.locale].details?.image?.height && {
+              height: mediaAsset.fields.file[environment.contentful.locale].details?.image?.height,
+            }),
+            fileName: mediaAsset.fields.file[environment.contentful.locale].fileName,
+            contentType: mediaAsset.fields.file[environment.contentful.locale].contentType,
+          }),
+        }
+      })
+      .filter(Boolean)
+
+    totalAssets.push(...normAssets)
+    assetsFetched += limit
+  }
 
   const filename = 'src/outputs/get-media-asset-details.csv'
   const writableStream = createWriteStream(filename)
 
   const columns = [
+    'id',
     'title',
     'description',
     'url',
@@ -48,7 +69,7 @@ export default async () => {
   ]
 
   const stringifier = stringify({ header: true, columns: columns })
-  normAssets.forEach((asset) => {
+  totalAssets.forEach((asset) => {
     stringifier.write(asset)
   })
 
