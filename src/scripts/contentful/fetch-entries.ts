c321d0771b15941e376773e771e737c7b94efba6
IgnacioNMiranda <ignacio.miranda@reign.cl>
2023-11-23 17:26:58 -0300

    feat: add fetch-entries script

---
+++ src/scripts/contentful/fetch-entries.ts
@@ -0,0 +1,26 @@
+import { environment } from '../../environment'
+import { contentfulClient } from '../../clients'
+import { Entry } from 'contentful-management'
+
+const limit = 1000
+
+const fetchedEntries: Entry[] = []
+
+export default async () => {
+  const space = await contentfulClient.getSpace(environment.contentful.spaceId)
+  const env = await space.getEnvironment(environment.contentful.env)
+
+  let entriesFetched = 0
+  while (true) {
+    const entries = await env.getEntries({
+      content_type: environment.contentful.contentType,
+      limit,
+      skip: entriesFetched,
+    })
+    if (entries.items.length === 0) break
+    fetchedEntries.push(...entries.items)
+    entriesFetched += limit
+  }
+
+  return fetchedEntries
+}
