82e42c8f52f16a8de689db4ac9baf6fb29ac0e4f
IgnacioNMiranda <i.miranda.f99@gmail.com>
2024-07-22 18:10:34 -0400

    feat: add get-all-products and delete-all-prices scripts

---
+++ src/scripts/commercetools/delete-all-prices.ts
@@ -0,0 +1,36 @@
+import { commercetoolsClient } from '../../clients/commercetools'
+import getAllProducts from './get-all-products'
+
+export default async () => {
+  try {
+    const products = await getAllProducts()
+
+    const priceMapping = products.map((product) => ({
+      id: product.id,
+      version: product.version,
+      prices: [product.masterVariant, ...product.variants]
+        .map((variant) => variant.prices?.map((price) => price.id))
+        .flat(Number.POSITIVE_INFINITY) as string[],
+    }))
+
+    for (let i = 0; i < priceMapping.length; i++) {
+      const priceItem = priceMapping[i]
+
+      await commercetoolsClient
+        .products()
+        .withId({ ID: priceItem.id })
+        .post({
+          body: {
+            version: priceItem.version,
+            actions: priceItem.prices.map((priceId) => ({
+              action: 'removePrice',
+              priceId,
+            })),
+          },
+        })
+        .execute()
+    }
+  } catch (error) {
+    console.error(JSON.stringify(error))
+  }
+}
---
+++ src/scripts/commercetools/get-all-products.ts
@@ -0,0 +1,34 @@
+import { ProductProjection } from '@commercetools/platform-sdk'
+import { commercetoolsClient } from '../../clients/commercetools'
+
+const limit = 500
+let offset = 0
+
+export default async () => {
+  try {
+    const products: ProductProjection[] = []
+    while (true) {
+      const productsResponse = await commercetoolsClient
+        .productProjections()
+        .get({
+          queryArgs: {
+            limit,
+            withTotal: true,
+            offset,
+          },
+        })
+        .execute()
+
+      products.push(...productsResponse.body.results)
+
+      offset += limit
+
+      if (offset >= (productsResponse.body.total ?? 0)) break
+    }
+
+    return products
+  } catch (error) {
+    console.error(JSON.stringify(error))
+    return []
+  }
+}
